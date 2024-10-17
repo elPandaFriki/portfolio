9fcf59eb923deb7e4461787ec5a2db6aaa71c84d
David Perera <davidpemo36@hotmail.com>
2024-10-17 10:00:12 +0200

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -4,64 +4,78 @@ import typescriptEslint from '@typescript/eslint'
 import globals from 'globals'
 import typescriptParser from '@typescript-eslint/parser'
 
-export default [{
-    globals: {
-        ...globals.browser,
-        ...globals.node
-    },
-    extends: [
-        "eslint:recommended",
-        "plugin:react/jsx-runtime",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react-hooks/recommended",
-        "prettier"
-    ],
-    root: true,
-    ignorePatterns: ["node_modules", "dist", "build", "scripts"],
-    languageOptions: {
-        parser: typescriptParser,
-        parserOptions: {
-            project: true,
-            tsconfigRootDir: "./",
-            ecmaVersion: "latest",
-            sourceType: "module"
-        }
-    },
-    plugins: {
-        react,
-        reactRefresh,
-        typescriptEslint
-    },
-    rules: {
-        "typescriptEslint/consistent-type-imports": [
-            2,
-            {
-                fixStyle: "separate-type-imports"
-            }
+export default [
+    {
+        files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
+        globals: {
+            ...globals.browser,
+            ...globals.node
+        },
+        extends: [
+            "eslint:recommended",
+            "plugin:react/jsx-runtime",
+            "plugin:@typescript-eslint/recommended",
+            "plugin:react-hooks/recommended",
+            "prettier"
         ],
-        "typescriptEslint/no-restricted-imports": [
-            2,
+        root: true,
+        ignorePatterns: [
+            "node_modules",
+            "dist",
+            "build",
+            "scripts"
+        ],
+        languageOptions: {
+            parser: typescriptParser,
+            parserOptions: {
+                project: true,
+                tsconfigRootDir: "./",
+                ecmaVersion: "latest",
+                sourceType: "module",
+                ecmaFeatures: {
+                    jsx: true
+                }
+            }
+        },
+        plugins: {
+            react,
+            reactRefresh,
+            typescriptEslint
+        },
+        rules: {
+            "typescriptEslint/consistent-type-imports": [
+                2,
+                {
+                    fixStyle: "separate-type-imports"
+                }
+            ],
+            "typescriptEslint/no-restricted-imports": [
+                2,
+                {
+                    paths: [
+                        {
+                            name: "react-redux",
+                            importNames: [
+                                "useSelector",
+                                "useStore",
+                                "useDispatch"
+                            ],
+                            message: "Please use pre-typed versions from `src/app/hooks.ts` instead."
+                        }
+                    ]
+                }
+            ]
+        },
+        overrides: [
             {
-                paths: [
-                    {
-                        name: "react-redux",
-                        importNames: ["useSelector", "useStore", "useDispatch"],
-                        message: "Please use pre-typed versions from `src/app/hooks.ts` instead."
-                    }
-                ]
+                files: ["*.{c,m,}{t,j}s", "*.{t,j}sx"]
+            },
+            {
+                files: ["*{test,spec}.{t,j}s?(x)"],
+                env: {
+                    jest: true
+                }
             }
         ]
-    },
-    overrides: [
-        {
-            files: ["*.{c,m,}{t,j}s", "*.{t,j}sx"]
-        },
-        {
-            files: ["*{test,spec}.{t,j}s?(x)"],
-            env: {
-                jest: true
-            }
-        }
-    ]
-}
+    }
 ]
